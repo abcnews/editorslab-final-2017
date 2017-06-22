c322a1dd5ace9355c5c0a0018b57bce8e3238d02
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-06-22 15:09:17 +0200

    Better landing page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', {title: 'News website'});
+  res.render('index', {title: 'Manifest'});
 });
 
 module.exports = router;
