b7c88391df75a5893fcedcbe4165902fbdb4df00
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-06-22 13:31:30 +0200

    Promisified app flow, loading cards before annotations (so they can be nested). Only runs on story pages.

--- public_src/index.js
+++ public_src/index.js
@@ -8,65 +8,114 @@ const qsa = document.querySelectorAll.bind(document);
 
 const storyEl = qs('.Story');
 
-storyEl.classList.add('is-hidden');
-setTimeout(() => {
-  storyEl.classList.remove('is-hidden');
-}, 250);
+if (storyEl) {
+  Promise.resolve()
+  .then(hideStory)
+  .then(loadCards)
+  .then(loadAnnotations)
+  .then(checkFamiliarity)
+  .then(showStory)
+  .catch(showStory);
+}
 
-const annotationRefs = slice(qsa('[data-annotation]')).reduce((memo, el) => {
-  memo[el.getAttribute('data-annotation')] = el;
-
-  return memo;
-}, {});
-
-const annotationRefsKeys = Object.keys(annotationRefs);
-
-if (annotationRefsKeys.length) {
-  const annotationsURL = `/annotations/?slugs=${annotationRefsKeys.join()}`;
-
-  fetch(annotationsURL)
-  .then(response => {
-    response.json().then(annotations => {
-      annotations.forEach(annotation => {
-        const refEl = annotationRefs[annotation.slug];
-        
-        if (refEl) {
-          Annotation(annotation, refEl);
-        }
-      });
-    });
+function hideStory() {
+  return new Promise(resolve => {
+    storyEl.classList.add('is-hidden');
+    resolve();
   });
 }
 
-const cardRefs = slice(qsa('[data-card]')).reduce((memo, el) => {
-  memo[el.getAttribute('data-card')] = el;
+function loadCards() {
+  return new Promise(resolve => {
+    const cardRefs = slice(qsa('[data-card]')).reduce((memo, el) => {
+      memo[el.getAttribute('data-card')] = el;
 
-  return memo;
-}, {});
+      return memo;
+    }, {});
 
-const cardRefsKeys = Object.keys(cardRefs);
+    const cardRefsKeys = Object.keys(cardRefs);
 
-if (cardRefsKeys.length) {
-  const cardsURL = `/cards/?slugs=${cardRefsKeys.join()}`;
+    if (cardRefsKeys.length) {
+      const cardsURL = `/cards/?slugs=${cardRefsKeys.join()}`;
 
-  fetch(cardsURL)
-  .then(response => {
-    response.json().then(cards => {
-      cards.forEach(card => {
-        const refEl = cardRefs[card.slug];
+      fetch(cardsURL)
+      .then(response => {
+        response.json()
+        .then(cards => {
+          cards.forEach(card => {
+            const refEl = cardRefs[card.slug];
+          
+            if (refEl) {
+              Card(card, refEl);
+            }
+          });
+
+          resolve();
+        })
+        .catch(resolve);
+      })
+      .catch(resolve);
+    } else {
+      resolve();
+    }
+  });
+}
+
+function loadAnnotations() {
+  return new Promise(resolve => {
+    const annotationRefs = slice(qsa('[data-annotation]')).reduce((memo, el) => {
+      memo[el.getAttribute('data-annotation')] = el;
+
+      return memo;
+    }, {});
+
+    const annotationRefsKeys = Object.keys(annotationRefs);
+
+    if (annotationRefsKeys.length) {
+      const annotationsURL = `/annotations/?slugs=${annotationRefsKeys.join()}`;
+
+      fetch(annotationsURL)
+      .then(response => {
+        response.json()
+        .then(annotations => {
+          annotations.forEach(annotation => {
+            const refEl = annotationRefs[annotation.slug];
+            
+            if (refEl) {
+              Annotation(annotation, refEl);
+            }
+          });
+
+          resolve();
+        })
+        .catch(resolve);
+      })
+      .catch(resolve);
+    } else {
+      resolve();
+    }
+  });
+}
+
+function checkFamiliarity() {
+  return new Promise(resolve => {
+    idbKeyval.keys()
+    .then(keys => {
+      const numCollapsedCards = keys.filter(key => key.indexOf('card-') > -1).length;
+
+      if (numCollapsedCards > 2) {
+        storyEl.classList.add('has-familiar-user');
+      }
       
-        if (refEl) {
-          Card(card, refEl);
-        }
-      });
-    });
+      resolve();
+    })
+    .catch(resolve);
   });
 }
 
-idbKeyval.keys().then(keys => {
-  const numCollapsedCards = keys.filter(key => key.indexOf('card-') > -1).length;
-
-  if (numCollapsedCards > 2) {
-    qs('.Story').classList.add('has-familiar-user');
-  }
-});
+function showStory() {
+  return new Promise(resolve => {
+    storyEl.classList.remove('is-hidden');
+    resolve();
+  });
+}
